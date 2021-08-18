abf1d008549a9204258c1ef27c1422df5ecf3965
Lai-YT <381xvmvbib@gmail.com>
2021-08-18 11:55:21 +0800

    std::thread

---
+++ multi_thread/thread/3_in_array.cpp
@@ -0,0 +1,30 @@
+/* g++ -std=c++11 3_in_array.cpp -o bin/in_array -pthread */
+
+#include <iostream>
+#include <thread>
+
+
+void Foo(const int n) {
+  std::cout << "foo " << n << '\n';
+}
+
+
+int main(int argc, char const *argv[]) {
+  /*
+   * Default is a thread object that does nothing,
+   * though it's still better to initialize at the same time.
+   */
+  std::thread threads[3];
+  /* initialization */
+  for (int i = 0; i < 3; ++i) {
+    threads[i] = std::thread(Foo, i);
+  }
+  /* make sure you join all of the threads */
+  for (int i = 0; i < 3; ++i) {
+    threads[i].join();
+  }
+
+  std::cout << "main exits." << '\n';
+
+  return 0;
+}
